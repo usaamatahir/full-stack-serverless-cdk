b36e4a7f3f66aada3ee8e0c86d8e86a631f72912
usaamatahir <usamatahir8330@gmail.com>
2020-12-02 16:56:29 +0500

    Added Types in LAmbda

--- step-02-lambda-function/lambda/hello.ts
+++ step-02-lambda-function/lambda/hello.ts
@@ -1,4 +1,13 @@
-exports.handler = async function (event: any) {
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+exports.handler = async function (
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
   console.log("request: ", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
